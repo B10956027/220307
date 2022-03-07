d38602cedb37a8db87b7b00d85b67850b7aff2c7
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:59:22 +0800

    練習

--- B10956027/Program.cs
+++ B10956027/Program.cs
@@ -6,7 +6,16 @@ namespace B10956027
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int meal = (int)meal_cost;
+            int tip_percent = 15;
+            int tax_percent = 8;
+
+            int total = meal+tip_percent+tax_percent;
+
+            Console.WriteLine("用餐總費用："+total);
+
+
         }
     }
 }
